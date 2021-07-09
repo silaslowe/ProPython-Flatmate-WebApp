a35d212b4c84b0ab35add235b11991b581909160
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-07-09 10:42:00 -0500

    created flask app and two web pages: home and bill

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask.views import MethodView
+from wtforms import Form
+from flask import Flask
+
+app = Flask(__name__)
+
+
+class HomePage(MethodView):
+
+    def get(self):
+        return "Hello There"
+
+
+class BillFormPage(MethodView):
+    def get(self):
+        return "I am the bill page"
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(Form):
+    pass
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run()
